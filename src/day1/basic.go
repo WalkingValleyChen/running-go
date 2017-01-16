c6b003b9095dec09c6419d0dc1a2796caaf1576f
chenwanli1111@foxmail.com <chenwanli1111@foxmail.com>
2017-01-16 23:07:14 +0800

    go first

---
+++ src/day1/basic.go
@@ -0,0 +1,29 @@
+package day1
+
+import (
+	"fmt"
+)
+
+func Iota() {
+	const (
+		a = iota   //0
+		b          //1
+		c          //2
+		d = "ha"   //独立值，iota += 1
+		e          //"ha"   iota += 1
+		f = 100    //iota +=1
+		g          //100  iota +=1
+		h = iota   //7,恢复计数
+		i          //8
+	)
+	fmt.Println(a, b, c, d, e, f, g, h, i)
+}
+
+func Function(a, b int) int {
+	if (a > b) {
+		return a;
+	} else {
+		return b;
+	}
+
+}
---
+++ src/main/main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+	"day1"
+)
+
+
+func main() {
+	fmt.Print("Hello world")
+	day1.Iota();
+	fmt.Print(day1.Function(1,2))
+}
